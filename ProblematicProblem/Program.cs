947c6ec89d8c473abdc467f219da6024d470e841
JustinBenning <justin.benning@gmail.com>
2024-02-02 10:58:18 -0500

    updated

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -161,7 +161,7 @@ namespace ProblematicProblem
 
                     } while (randomActivity == "Wine Tasting");
 
-                    Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                    Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
                     
                     do
                     {
